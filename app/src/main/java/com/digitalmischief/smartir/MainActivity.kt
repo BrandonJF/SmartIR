27b1b6179244ba0b5ae14a963714012a50c69c74
Brandon John-Freso <brandonjf@gmail.com>
2017-09-16 16:26:32 -0400

    pushing changes that fix the short circuiting of the gpio listener.

--- app/src/main/java/com/digitalmischief/smartir/MainActivity.kt
+++ app/src/main/java/com/digitalmischief/smartir/MainActivity.kt
@@ -4,13 +4,18 @@ import android.app.Activity
 import android.os.Bundle
 import android.util.Log
 import com.google.android.things.contrib.driver.button.Button
+import com.google.android.things.pio.Gpio
+import com.google.android.things.pio.GpioCallback
 import com.google.android.things.pio.PeripheralManagerService
 
 private val TAG = MainActivity::class.java.simpleName
 private val gpioButtonPinName = "BUS NAME"
-private val GPIO_IR_PORT = "BCM17"
+private val GPIO_BUTTON_PORT = "BCM13"
+private val GPIO_LED_PORT = "BCM17"
 
 val manager = PeripheralManagerService()
+var buttonGpio: Gpio? = null
+var ledGpio: Gpio? = null
 
 class MainActivity : Activity() {
     private lateinit var mButton: Button
@@ -22,7 +27,7 @@ class MainActivity : Activity() {
 
     override fun onDestroy() {
         super.onDestroy()
-//        destroyButton()
+        destroyGpio()
     }
 
     private fun setupPeripherals() {
@@ -31,14 +36,59 @@ class MainActivity : Activity() {
             Log.d(TAG, "Cannot find any GPIO ports")
         } else {
             Log.d(TAG, "List of available ports: $portList")
-//            setupIRInput()
+            setupGPIO()
         }
 
     }
-//
-//    private fun setupIRInput() {
-//        val receiverGpiomanager.openGpio(GPIO_IR_PORT)
-//    }
+
+    private fun setupGPIO() {
+
+
+        buttonGpio = manager.openGpio(GPIO_BUTTON_PORT)
+        buttonGpio?.let{
+            it.setDirection(Gpio.DIRECTION_IN)
+            it.setEdgeTriggerType(Gpio.EDGE_FALLING)
+            it.registerGpioCallback(mGpioCallback)
+        }
+
+        ledGpio = manager.openGpio(GPIO_LED_PORT)
+        ledGpio?.let {
+            it.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
+        }
+
+
+
+
+    }
+
+    private val mGpioCallback = object : GpioCallback() {
+        override fun onGpioEdge(gpio: Gpio?): Boolean {
+            // Read the active low pin state
+            if (gpio!!.value) {
+                Log.d(TAG, "Reading a low value.")
+            } else {
+                // Pin is HIGH
+                Log.d(TAG, "Reading a high value.")
+                ledGpio!!.value = !ledGpio!!.value
+            }
+
+            // Continue listening for more interrupts
+            return true
+        }
+
+        override fun onGpioError(gpio: Gpio?, error: Int) {
+            Log.w(TAG, gpio.toString() + ": Error event " + error)
+        }
+    }
+    private fun destroyGpio() {
+        buttonGpio?.let {
+            it.close()
+            it.unregisterGpioCallback(mGpioCallback)
+            buttonGpio = null
+
+        }
+
+    }
 
 //    private fun setupButton() {
 //        try {
